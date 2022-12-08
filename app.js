7677086ea38d80bcf1f33e7b98560bb2d2abc6f4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-08 14:11:19 -0500

    Add graphql

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const auth = require('./middleware/isAuth')
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const jwt = require('jsonwebtoken')
+
+
+module.exports = {}
